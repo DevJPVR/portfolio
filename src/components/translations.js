1e37b419756de320fa505e7d238586c28364f971
João Santos <joaosantos@JoaoFrontEnd.lan>
2023-07-20 15:54:44 +0100

    new translation

--- src/components/translations.js
+++ src/components/translations.js
@@ -19,18 +19,6 @@ var linguJSON = {
   	"custom_lang_switcher_html": "<div id=\"langswitcher\" class=\"notranslate\"> <a id=\"currlanglink\" href=\"\">[[linguCurrLang]]</a> <a href=\"[[linguTargetLangHref1]]\" class=\"targetlang\">[[linguTargetLang1]]</a><a href=\"[[linguTargetLangHref2]]\" class=\"targetlang\">[[linguTargetLang2]]</a></div>",
     "custom_lang_switcher_container_id": "langlinks",
     "translated_segments": [
-        // INICIO MENU 
-        {
-            "source": "Agenda Notícias, Desenvolvimento, Áreas de negócio Produtos, Contactos",
-            "target_en": "Agenda News, Development, Business Areas Products, Contacts",
-            "target_es": "Agenda Noticias, Desarrollo, Áreas de Negocio Productos, Contactos"
-        },
-     	{
-            "source": "Portugues",
-            "target_en": "English",
-            "target_es": "Espanhol"
-        },
-         //FIM MENU
          {
             "source": "+450 Produtos",
             "target_en": "+450 Products",
