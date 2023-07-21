d3d7aeebc1171c6730bd86fcf10c2bc2fd291ffe
João Santos <joaosantos@JoaoFrontEnd.lan>
2023-07-21 10:37:46 +0100

    translation

---
+++ src/translations.js
@@ -0,0 +1,120 @@
+var linguJSON = {
+    "languages": [
+    		{
+            "lang_name": "Portuguêse",
+            "lang_code": "pt",
+            "url_pattern": "?"
+        },
+    		{
+            "lang_name": "English",
+            "lang_code": "en",
+            "url_pattern": "?"
+        },
+        {
+            "lang_name": "Espanhol",
+            "lang_code": "es",
+            "url_pattern": "?"
+        }
+    ],
+  	"custom_lang_switcher_html": "<div id=\"langswitcher\" class=\"notranslate\"> <a id=\"currlanglink\" href=\"\">[[linguCurrLang]]</a> <a href=\"[[linguTargetLangHref1]]\" class=\"targetlang\">[[linguTargetLang1]]</a><a href=\"[[linguTargetLangHref2]]\" class=\"targetlang\">[[linguTargetLang2]]</a></div>",
+    "custom_lang_switcher_container_id": "langlinks",
+    "translated_segments": [
+          {
+            "source": "Agenda | Notícias",
+            "target_en": "Schedule | News",
+            "target_es": "Horario | Noticias"
+        },
+        {
+            "source": "Desenvolvimento",
+            "target_en": "Development",
+            "target_es": "Desarrollo"
+        },
+        {
+            "source": "Áreas de negócio | Produtos",
+            "target_en": "Business Areas | Products",
+            "target_es": "Áreas de negocio | productos"
+        },
+        {
+            "source": "Contactos",
+            "target_en": "Contacts",
+            "target_es": "Contactos"
+        },
+       
+     		{
+            "source": "Portugues",
+            "target_en": "English",
+            "target_es": "Espanhol"
+        },
+       {
+            "source": "+450 Produtos",
+            "target_en": "+450 Products",
+            "target_es": "+450 Productos"
+        }, 
+        {
+            "source": "Mais de 450 moldes diferentes.",
+            "target_en": "Over 450 different templates.",
+            "target_es": "Más de 450 plantillas diferentes."
+        },
+        {
+            "source": "+400 Clientes 2022",
+            "target_en": "+400 Clients 2022",
+            "target_es": "+400 Clientes 2022"
+        },
+        {
+            "source": "Mais de 400 empresas confiam em nós para dar resposta às suas necessidades.",
+            "target_en": "More than 400 companies trust us to meet their needs.",
+            "target_es": "Más de 400 empresas confían en nosotros para satisfacer sus necesidades."
+        },
+
+        {
+            "source": "+60 Colaboradores",
+            "target_en": "+60 Collaborators",
+            "target_es": "+60 Colaboradores"
+        },
+        {
+            "source": "A nossa equipa é composta por mais de 60 colaboradores.",
+            "target_en": "Our team consists of more than 60 collaborators.",
+            "target_es": "Nuestro equipo está compuesto por más de 60 colaboradores."
+        },
+        {
+            "source": "Vendas 2022",
+            "target_en": "Sales 2022",
+            "target_es": "Ventas 2022"
+        },
+        {
+            "source": "Ao respeitarmos o princípio de um crescimento sustentável atingimos 7,5 milhões em vendas no ano de 2022.",
+            "target_en": "By adhering to the principle of sustainable growth, we reached 7.5 million in sales in the year 2022.",
+            "target_es": "Al adherirnos al principio de crecimiento sostenible, alcanzamos 7.5 millones en ventas en el año 2022."
+        },
+        {
+            "source": "Reputação",
+            "target_en": "Reputation",
+            "target_es": "Reputación"
+        },
+        {
+            "source": "Somos reconhecidos como um fornecedor de soluções inovadoras.",
+            "target_en": "We are recognized as a supplier of innovative solutions.",
+            "target_es": "Somos reconocidos como un proveedor de soluciones innovadoras."
+        },
+        {
+            "source": "A Plásticos Futura dedica-se à transformação de matéria plástica, pelo processo de Injecção, Sopro, Injecção-Sopro e Termoformagem. Produzindo assim embalagens, acessórios e componentes em plástico para as Industrias Alimentar, Catering, Médico-farmacêutica, Cosmética, Auto, entre outras.",
+            "target_en": "Plásticos Futura is dedicated to the transformation of plastic material through Injection, Blow Molding, Injection-Blow Molding, and Thermoforming processes. Thus producing plastic packaging, accessories, and components for Food, Catering, Medical-Pharmaceutical, Cosmetic, Automotive industries, among others.",
+            "target_es": "Plásticos Futura se dedica a la transformación de materia plástica mediante los procesos de Inyección, Soplado, Inyección-Soplado y Termoformado. Producendo así envases, accesorios y componentes de plástico para las industrias alimentarias, de catering, médico-farmacéuticas, cosméticas, automotrices, entre otras."
+        },
+        {
+       		 "source": "Contando já com mais de 4 décadas de actividade, a Plásticos Futura orgulha-se do seu constante crescimento sustentado, assente em inovação tecnológica e métodos diferenciadores, o que lhe permite a actual posição de destaque no sector e reconhecimento como PME líder.",
+       		 "target_en": "With over 4 decades of activity, Plásticos Futura takes pride in its constant and sustained growth, based on technological innovation and distinctive methods, which have led it to its current prominent position in the sector and recognition as a leading SME.",
+       		 "target_es": "Con más de 4 décadas de actividad, Plásticos Futura se enorgullece de su crecimiento constante y sostenido, basado en la innovación tecnológica y métodos distintivos, lo que le ha permitido alcanzar una posición destacada en el sector y ser reconocida como una PYME líder."
+        },
+        {
+        	"source": "O seu melhor parceiro na investigação e desenvolvimento de produtos em plástico.",
+        	"target_en": "Your best partner in plastic product research and development.",
+        	"target_es": "Su mejor socio en investigación y desarrollo de productos de plástico."
+        },
+        {
+          "source": "O seu melhor parceiro na <strong>investigação</strong> e <strong>desenvolvimento</strong> de <strong>soluções em plástico</strong>.",
+          "target_en": "Your best partner in <strong>research</strong> and <strong>development</strong> of <strong>plastic solutions</strong>.",
+          "target_es": "Su mejor socio en <strong>investigación</strong> y <strong>desarrollo</strong> de <strong>soluciones en plástico</strong>."
+        }
+    ]
+};
