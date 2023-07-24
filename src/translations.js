8465e1611476aaef646ec70304735cb7ad438ee6
João Santos <joaosantos@JoaoFrontEnd.lan>
2023-07-24 17:12:07 +0100

    translation

--- src/translations.js
+++ src/translations.js
@@ -1,120 +1,185 @@
 var linguJSON = {
-    "languages": [
-    		{
-            "lang_name": "Portuguêse",
-            "lang_code": "pt",
-            "url_pattern": "?"
-        },
-    		{
-            "lang_name": "English",
-            "lang_code": "en",
-            "url_pattern": "?"
-        },
-        {
-            "lang_name": "Espanhol",
-            "lang_code": "es",
-            "url_pattern": "?"
-        }
-    ],
-  	"custom_lang_switcher_html": "<div id=\"langswitcher\" class=\"notranslate\"> <a id=\"currlanglink\" href=\"\">[[linguCurrLang]]</a> <a href=\"[[linguTargetLangHref1]]\" class=\"targetlang\">[[linguTargetLang1]]</a><a href=\"[[linguTargetLangHref2]]\" class=\"targetlang\">[[linguTargetLang2]]</a></div>",
-    "custom_lang_switcher_container_id": "langlinks",
-    "translated_segments": [
-          {
-            "source": "Agenda | Notícias",
-            "target_en": "Schedule | News",
-            "target_es": "Horario | Noticias"
-        },
-        {
-            "source": "Desenvolvimento",
-            "target_en": "Development",
-            "target_es": "Desarrollo"
-        },
-        {
-            "source": "Áreas de negócio | Produtos",
-            "target_en": "Business Areas | Products",
-            "target_es": "Áreas de negocio | productos"
-        },
-        {
-            "source": "Contactos",
-            "target_en": "Contacts",
-            "target_es": "Contactos"
-        },
-       
-     		{
-            "source": "Portugues",
-            "target_en": "English",
-            "target_es": "Espanhol"
-        },
-       {
-            "source": "+450 Produtos",
-            "target_en": "+450 Products",
-            "target_es": "+450 Productos"
-        }, 
-        {
-            "source": "Mais de 450 moldes diferentes.",
-            "target_en": "Over 450 different templates.",
-            "target_es": "Más de 450 plantillas diferentes."
-        },
-        {
-            "source": "+400 Clientes 2022",
-            "target_en": "+400 Clients 2022",
-            "target_es": "+400 Clientes 2022"
-        },
-        {
-            "source": "Mais de 400 empresas confiam em nós para dar resposta às suas necessidades.",
-            "target_en": "More than 400 companies trust us to meet their needs.",
-            "target_es": "Más de 400 empresas confían en nosotros para satisfacer sus necesidades."
-        },
+  "languages": [
+      {
+          "lang_name": "Português",
+          "lang_code": "pt",
+          "url_pattern": "?"
+      },
+      {
+          "lang_name": "English",
+          "lang_code": "en",
+          "url_pattern": "?"
+      },
+      {
+          "lang_name": "Espanhol",
+          "lang_code": "es",
+          "url_pattern": "?"
+      },
+      {
+          "lang_name": "Francês",
+          "lang_code": "fr",
+          "url_pattern": "?"
+      }
+  ],
+  "custom_lang_switcher_html": "<div id=\"langswitcher\" class=\"notranslate\"><a id=\"currlanglink\" href=\"\">[[linguCurrLang]]</a><a href=\"[[linguTargetLangHref1]]\" class=\"targetlang\">[[linguTargetLang1]]</a><a href=\"[[linguTargetLangHref2]]\" class=\"targetlang\">[[linguTargetLang2]]</a> <a href=\"[[linguTargetLangHref3]]\" class=\"targetlang\">[[linguTargetLang3]]</a></div>",
+  "custom_lang_switcher_container_id": "langlinks",
+  "translated_segments": [
+{
+"source": "Agenda | Notícias",
+"target_en": "Schedule | News",
+"target_es": "Horario | Noticias",
+"target_fr": "Agenda | Actualités"
+},
+{
+"source": "Desenvolvimento",
+"target_en": "Development",
+"target_es": "Desarrollo",
+"target_fr": "Développement"
+},
+{
+"source": "Áreas de negócio | Produtos",
+"target_en": "Business Areas | Products",
+"target_es": "Áreas de negocio | productos",
+"target_fr": "Domaines d'activité | Produits"
+},
+{
+"source": "Contactos",
+"target_en": "Contacts",
+"target_es": "Contactos",
+"target_fr": "Contacts"
+},
+{
+"source": "Portugues",
+"target_en": "English",
+"target_es": "Espanhol",
+"target_fr": "Français"
+},
+{
+"source": "+450 Produtos",
+"target_en": "+450 Products",
+"target_es": "+450 Productos",
+"target_fr": "+450 Produits"
+},
+{
+"source": "Mais de 450 moldes diferentes.",
+"target_en": "Over 450 different templates.",
+"target_es": "Más de 450 plantillas diferentes.",
+"target_fr": "Plus de 450 modèles différents."
+},
+{
+"source": "+400 Clientes 2022",
+"target_en": "+400 Clients 2022",
+"target_es": "+400 Clientes 2022",
+"target_fr": "+400 Clients 2022"
+},
+{
+"source": "Mais de 400 empresas confiam em nós para dar resposta às suas necessidades.",
+"target_en": "More than 400 companies trust us to meet their needs.",
+"target_es": "Más de 400 empresas confían en nosotros para satisfacer sus necesidades.",
+"target_fr": "Plus de 400 entreprises nous font confiance pour répondre à leurs besoins."
+},
+{
+"source": "+60 Colaboradores",
+"target_en": "+60 Collaborators",
+"target_es": "+60 Colaboradores",
+"target_fr": "+60 Collaborateurs"
+},
+{
+"source": "A nossa equipa é composta por mais de 60 colaboradores.",
+"target_en": "Our team consists of more than 60 collaborators.",
+"target_es": "Nuestro equipo está compuesto por más de 60 colaboradores.",
+"target_fr": "Notre équipe est composée de plus de 60 collaborateurs."
+},
+{
+"source": "Vendas 2022",
+"target_en": "Sales 2022",
+"target_es": "Ventas 2022",
+"target_fr": "Ventes 2022"
+},
+{
+"source": "Ao respeitarmos o princípio de um crescimento sustentável atingimos 7,5 milhões em vendas no ano de 2022.",
+"target_en": "By adhering to the principle of sustainable growth, we reached 7.5 million in sales in the year 2022.",
+"target_es": "Al adherirnos al principio de crecimiento sostenible, alcanzamos 7.5 millones en ventas en el año 2022.",
+"target_fr": "En respectant le principe de croissance durable, nous avons atteint 7,5 millions de ventes en 2022."
+},
+{
+"source": "Reputação",
+"target_en": "Reputation",
+"target_es": "Reputación",
+"target_fr": "Réputation"
+},
+{
+"source": "Somos reconhecidos como um fornecedor de soluções inovadoras.",
+"target_en": "We are recognized as a supplier of innovative solutions.",
+"target_es": "Somos reconocidos como un proveedor de soluciones innovadoras.",
+"target_fr": "Nous sommes reconnus comme un fournisseur de solutions innovantes."
+},
+{
+"source": "A Plásticos Futura dedica-se à transformação de matéria plástica, pelo processo de Injecção, Sopro, Injecção-Sopro e Termoformagem. Produzindo assim embalagens, acessórios e componentes em plástico para as Industrias Alimentar, Catering, Médico-farmacêutica, Cosmética, Auto, entre outras.",
+"target_en": "Plásticos Futura is dedicated to the transformation of plastic material through Injection, Blow Molding, Injection-Blow Molding, and Thermoforming processes. Thus producing plastic packaging, accessories, and components for Food, Catering, Medical-Pharmaceutical, Cosmetic, Automotive industries, among others.",
+"target_es": "Plásticos Futura se dedica a la transformación de materia plástica mediante los procesos de Inyección, Soplado, Inyección-Soplado y Termoformado. Producendo así envases, accesorios y componentes de plástico para las industrias alimentarias, de catering, médico-farmacéuticas, cosméticas, automotrices, entre otras.",
+"target_fr": "Plásticos Futura est spécialisée dans la transformation de matériaux plastiques par les procédés d'injection, de soufflage, d'injection-soufflage et de thermoformage. Elle produit ainsi des emballages, des accessoires et des composants en plastique pour les industries alimentaires, de restauration, médico-pharmaceutiques, cosmétiques, automobiles, entre autres."
+},
+{
+"source": "Contando já com mais de 4 décadas de actividade, a Plásticos Futura orgulha-se do seu constante crescimento sustentado, assente em inovação tecnológica e métodos diferenciadores, o que lhe permite a actual posição de destaque no sector e reconhecimento como PME líder.",
+"target_en": "With over 4 decades of activity, Plásticos Futura takes pride in its constant and sustained growth, based on technological innovation and distinctive methods, which have led it to its current prominent position in the sector and recognition as a leading SME.",
+"target_es": "Con más de 4 décadas de actividad, Plásticos Futura se enorgullece de su crecimiento constante y sostenido, basado en la innovación tecnológica y métodos distintivos, lo que le ha permitido alcanzar una posición destacada en el sector y ser reconocida como una PYME líder.",
+"target_fr": "Avec plus de 4 décennies d'activité, Plásticos Futura est fière de sa croissance constante et soutenue, basée sur l'innovation technologique et des méthodes distinctives, ce qui lui a permis d'occuper une position de premier plan dans le secteur et d'être reconnue comme une PME leader."
+},
+{
+"source": "O seu melhor parceiro na investigação e desenvolvimento de produtos em plástico.",
+"target_en": "Your best partner in plastic product research and development.",
+"target_es": "Su mejor socio en investigación y desarrollo de productos de plástico.",
+"target_fr": "Votre meilleur partenaire pour la recherche et le développement de produits en plastique."
+},
+{
+"source": "O seu melhor parceiro na <strong>investigação</strong> e <strong>desenvolvimento</strong> de <strong>soluções em plástico</strong>.",
+"target_en": "Your best partner in <strong>research</strong> and <strong>development</strong> of <strong>plastic solutions</strong>.",
+"target_es": "Su mejor socio en <strong>investigación</strong> y <strong>desarrollo</strong> de <strong>soluciones en plástico</strong>.",
+"target_fr": "Votre meilleur partenaire pour la <strong>recherche</strong> et le <strong>développement</strong> de <strong>solutions en plastique</strong>."
+},
+{
+"source": "Nossa História",
+"target_en": "Our Story",
+"target_es": "Nuestra Historia",
+"target_fr": "Notre Histoire"
+},
+{
+"source": "Saltar História",
+"target_en": "Skip History",
+"target_es": "Saltar Historia",
+"target_fr": "Passer l'Histoire"
+},
+{
+"source": "Nasceu a Plásticos Futura! Inicio de produção e fornecimento de embalagens para alimentos.",
+"target_en": "Plásticos Futura was born! Start of production and supply of packaging for food.",
+"target_es": "¡Nació Plásticos Futura! Inicio de producción y suministro de envases para alimentos.",
+"target_fr": "Plásticos Futura est née ! Début de la production et de l'approvisionnement d'emballages pour aliments."
+},
+{
+"source": "Inauguração das novas instalações.",
+"target_en": "Inauguration of the new facilities.",
+"target_es": "Inauguración de las nuevas instalaciones.",
+"target_fr": "Inauguration des nouvelles installations."
+},
+{
+"source": "Primeira exportação para a Europa. Atualmente, daremos resposta às necessidades de mais de 50 países.",
+"target_en": "First export to Europe. Currently, we meet the needs of more than 50 countries.",
+"target_es": "Primera exportación a Europa. Actualmente, atendemos las necesidades de más de 50 países.",
+"target_fr": "Première exportation vers l'Europe. Actuellement, nous répondons aux besoins de plus de 50 pays."
+},
+{
+"source": "Certificação ISO 9001.",
+"target_en": "ISO 9001 Certification.",
+"target_es": "Certificación ISO 9001.",
+"target_fr": "Certification ISO 9001."
+},
+{
+"source": "Primeira linha de máquinas de sopro com tecnologia PET.",
+"target_en": "First line of blow molding machines with PET technology.",
+"target_es": "Primera línea de máquinas de soplado con tecnología PET.",
+"target_fr": "Première ligne de machines de soufflage avec technologie PET."
+}
 
-        {
-            "source": "+60 Colaboradores",
-            "target_en": "+60 Collaborators",
-            "target_es": "+60 Colaboradores"
-        },
-        {
-            "source": "A nossa equipa é composta por mais de 60 colaboradores.",
-            "target_en": "Our team consists of more than 60 collaborators.",
-            "target_es": "Nuestro equipo está compuesto por más de 60 colaboradores."
-        },
-        {
-            "source": "Vendas 2022",
-            "target_en": "Sales 2022",
-            "target_es": "Ventas 2022"
-        },
-        {
-            "source": "Ao respeitarmos o princípio de um crescimento sustentável atingimos 7,5 milhões em vendas no ano de 2022.",
-            "target_en": "By adhering to the principle of sustainable growth, we reached 7.5 million in sales in the year 2022.",
-            "target_es": "Al adherirnos al principio de crecimiento sostenible, alcanzamos 7.5 millones en ventas en el año 2022."
-        },
-        {
-            "source": "Reputação",
-            "target_en": "Reputation",
-            "target_es": "Reputación"
-        },
-        {
-            "source": "Somos reconhecidos como um fornecedor de soluções inovadoras.",
-            "target_en": "We are recognized as a supplier of innovative solutions.",
-            "target_es": "Somos reconocidos como un proveedor de soluciones innovadoras."
-        },
-        {
-            "source": "A Plásticos Futura dedica-se à transformação de matéria plástica, pelo processo de Injecção, Sopro, Injecção-Sopro e Termoformagem. Produzindo assim embalagens, acessórios e componentes em plástico para as Industrias Alimentar, Catering, Médico-farmacêutica, Cosmética, Auto, entre outras.",
-            "target_en": "Plásticos Futura is dedicated to the transformation of plastic material through Injection, Blow Molding, Injection-Blow Molding, and Thermoforming processes. Thus producing plastic packaging, accessories, and components for Food, Catering, Medical-Pharmaceutical, Cosmetic, Automotive industries, among others.",
-            "target_es": "Plásticos Futura se dedica a la transformación de materia plástica mediante los procesos de Inyección, Soplado, Inyección-Soplado y Termoformado. Producendo así envases, accesorios y componentes de plástico para las industrias alimentarias, de catering, médico-farmacéuticas, cosméticas, automotrices, entre otras."
-        },
-        {
-       		 "source": "Contando já com mais de 4 décadas de actividade, a Plásticos Futura orgulha-se do seu constante crescimento sustentado, assente em inovação tecnológica e métodos diferenciadores, o que lhe permite a actual posição de destaque no sector e reconhecimento como PME líder.",
-       		 "target_en": "With over 4 decades of activity, Plásticos Futura takes pride in its constant and sustained growth, based on technological innovation and distinctive methods, which have led it to its current prominent position in the sector and recognition as a leading SME.",
-       		 "target_es": "Con más de 4 décadas de actividad, Plásticos Futura se enorgullece de su crecimiento constante y sostenido, basado en la innovación tecnológica y métodos distintivos, lo que le ha permitido alcanzar una posición destacada en el sector y ser reconocida como una PYME líder."
-        },
-        {
-        	"source": "O seu melhor parceiro na investigação e desenvolvimento de produtos em plástico.",
-        	"target_en": "Your best partner in plastic product research and development.",
-        	"target_es": "Su mejor socio en investigación y desarrollo de productos de plástico."
-        },
-        {
-          "source": "O seu melhor parceiro na <strong>investigação</strong> e <strong>desenvolvimento</strong> de <strong>soluções em plástico</strong>.",
-          "target_en": "Your best partner in <strong>research</strong> and <strong>development</strong> of <strong>plastic solutions</strong>.",
-          "target_es": "Su mejor socio en <strong>investigación</strong> y <strong>desarrollo</strong> de <strong>soluciones en plástico</strong>."
-        }
-    ]
+  ]
 };
